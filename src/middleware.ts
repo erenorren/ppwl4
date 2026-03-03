388c5e1d1b2b0243106dabac4120e5ca368ef876
erenorren <regishashrn07@gmail.com>
2026-03-04 06:54:27 +0700

    commit

---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { Elysia } from "elysia"
+import { openapi } from "@elysiajs/openapi";
+
+
+const app = new Elysia()
+.use(openapi())
+// Global Logger
+app.onRequest(({ request }) => {
+ console.log("📥", request.method, request.url)
+ console.log("🕒", new Date().toISOString())
+})
+
+
+app.get("/", () => "Hello Middleware")
+
+app.onRequest(({ request, set }) => {
+  if (request.headers.get("x-block") === "true") {
+    set.status = 403
+    return { message: "Blocked" }
+  }
+})
+
+app.get(
+ "/dashboard",
+ () => ({
+   message: "Welcome to Dashboard"
+ }),
+ {
+   beforeHandle({ headers, set }) {
+     if (!headers.authorization) {
+       set.status = 401
+       return {
+         success: false,
+         message: "Unauthorized"
+       }
+     }
+   }
+ }
+)
+
+app.listen(3000)
+console.log("Server running at http://localhost:3000")
