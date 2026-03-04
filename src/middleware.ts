8ad2e8dba97e3cc83f56cec4aef133fe4b2aa568
erenorren <regishashrn07@gmail.com>
2026-03-04 20:49:03 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,42 +1,61 @@
-import { Elysia } from "elysia"
+// Praktikum 4, 5, 6
+
+import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
-
 const app = new Elysia()
-.use(openapi())
-// Global Logger
-app.onRequest(({ request }) => {
- console.log("📥", request.method, request.url)
- console.log("🕒", new Date().toISOString())
-})
+  .use(openapi())
+  .get(
+    "/admin",
+    () => {
+      return { stats: 99 };
+    },
+    {beforeHandle({ headers, set }) {
+        if (headers.authorization !== "Bearer 123") {
+          set.status = 401;
+          return {
+            success: false,
+            message: "Unauthorized" };
+        }
+      }
+    }
+  )
 
 
-app.get("/", () => "Hello Middleware")
+    app.onAfterHandle(({ response }) => {
+    return {
+      success: true,
+      message: "data tersedia",
+      data: response
+    }
+  })
 
-app.onRequest(({ request, set }) => {
-  if (request.headers.get("x-block") === "true") {
-    set.status = 403
-    return { message: "Blocked" }
-  }
-})
+  app.get("/product", () => {
+    return { id: 1, name: "Laptop" }
+  })
 
-app.get(
- "/dashboard",
- () => ({
-   message: "Welcome to Dashboard"
- }),
- {
-   beforeHandle({ headers, set }) {
-     if (!headers.authorization) {
-       set.status = 401
-       return {
-         success: false,
-         message: "Unauthorized"
-       }
-     }
-   }
- }
-)
+  app.post("/login", ({ body }) => body, {
+    body: t.Object({
+      email: t.String({ format: "email" }),
+      password: t.String({ minLength: 8 })
+    })
+  })
 
-app.listen(3000)
-console.log("Server running at http://localhost:3000")
+  app.onError(({ code, error, set }) => {
+    if (code === "VALIDATION") {
+      set.status = 400
+      return {
+        success: false,
+        error: "Validation Error"
+      }
+    }
+    
+    if (code === "NOT_FOUND") {
+      set.status = 404
+      return { message: "Route not found" }
+    }
+  })
+
+  .listen(3000);
+
+console.log(`Server running at ${app.server?.hostname}:${app.server?.port}`);
--- src/validation.ts
+++ src/validation.ts
@@ -1,41 +1,56 @@
+//Praktikum 1, 2, 3
+
 import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
 const app = new Elysia()
-  .use(openapi())
-  .post("/request",
-    ({ body }) => {
-      return {
-        message: "Success",
-        data: body
-      }
-    },
-    {
-      body: t.Object({
-        name: t.String({ minLength: 3 }),
-        email: t.String({ format: "email" }),
-        age: t.Number({ minimum: 18 })
-      })
-    }
-  )
-  .listen(3000);
-
-  app.get(
-  "/ping",
-  () => {
+.use(openapi())
+.post(
+  "/request",
+  ({ body }) => {
     return {
-      success: true,
-      message: "Server OK"
-    }
+      message: "Success",
+      data: body
+    };
   },
   {
-    response: t.Object({
-      success: t.Boolean(),
-      message: t.String()
+    body: t.Object({
+      name: t.String({ minLength: 3 }),
+      email: t.String({ format: "email" }),
+      age: t.Number({ minimum: 18 })
     })
   }
 )
 
+.get("/products/:id", ({ params, query }) => {
+    return {
+        id: params.id,
+        sort: query.sort
+    }
+}, {
+    params: t.Object({
+        id: t.Number() // Ketentuan: Id harus berupa number 
+    }),
+    query: t.Object({
+        sort: t.Optional(
+            t.String({ 
+                pattern: "^(asc|desc)$" // Ketentuan: Sort hanya boleh "asc" atau "desc" 
+            })
+        )
+    })
+})
 
+.get("/stats", () => {
+    return {
+        total: 100,
+        active: 50
+    }
+}, {
+    response: t.Object({
+        total: t.Number(),
+        active: t.Number()
+    })
+})
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+.listen(3000);
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
